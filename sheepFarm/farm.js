2a6d8ceb523bb93ad87b28d8a933bb7c54defd84
sametkoyuncu <sametkoyuncu@live.com>
2021-08-15 22:38:05 +0300

    Moved some mothods from farm to user

--- sheepFarm/farm.js
+++ sheepFarm/farm.js
@@ -1,7 +1,3 @@
-const User = require('./user')
-const Sheep = require('./sheep')
-const Task = require('./task')
-
 class Farm {
     constructor(name, addrress, phone) {
         this.name = name
@@ -10,27 +6,6 @@ class Farm {
         this.users = []
         this.tasks = []
         this.sheeps = []
-    }
-
-    addUser(name, email, password, role, farm) {
-        const user = new User(name, email, password, role, farm)
-        this.users.push(user)
-
-        return user
-    }
-    
-    addSheep(farmId, officialId, bornDate, gender, race, mother, father) {
-        const sheep = new Sheep(farmId, officialId, bornDate, gender, race, mother, father)
-        this.sheeps.push(sheep)
-
-        return sheep
-    }
-
-    addTask(header, text, forWhom, whoDidIt, timeTodo, timeDone, Status) {
-        const task = new Task(header, text, forWhom, whoDidIt, timeTodo, timeDone, Status)
-        this.tasks.push(task)
-
-        return task
     }
 }
 
--- sheepFarm/user.js
+++ sheepFarm/user.js
@@ -1,3 +1,7 @@
+const User = require('./user')
+const Sheep = require('./sheep')
+const Task = require('./task')
+
 class User {
     constructor(name, email, password, role, farm) {
         this.name = name
@@ -5,6 +9,27 @@ class User {
         this.password = password
         this.role = role
         this.farm = farm
+    }
+
+    addUser(name, email, password, role, farm) {
+        const user = new User(name, email, password, role, farm)
+        this.farm.users.push(user)
+
+        return user
+    }
+
+    addSheep(farmId, officialId, bornDate, gender, race, mother, father) {
+        const sheep = new Sheep(farmId, officialId, bornDate, gender, race, mother, father)
+        this.farm.sheeps.push(sheep)
+
+        return sheep
+    }
+
+    addTask(header, text, forWhom, whoDidIt, timeTodo, timeDone, Status) {
+        const task = new Task(header, text, forWhom, whoDidIt, timeTodo, timeDone, Status)
+        this.farm.tasks.push(task)
+
+        return task
     }
 }
 //addedBy ?? for employees

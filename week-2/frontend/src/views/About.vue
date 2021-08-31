79983820a40e952c615a2322134f516b964df082
Samet Koyuncu <sametkoyuncu@live.com>
2021-08-31 15:20:43 +0300

    End of 'Vue.js'e giriş' lesson

--- week-2/frontend/src/views/About.vue
+++ week-2/frontend/src/views/About.vue
@@ -1,5 +1,18 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'About',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template lang="pug">
+  .about
+    h1 This is an about page
+    HelloWorld(msg="This is İstanbul!")
   </div>
 </template>
